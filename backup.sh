579f4b02ffb6ec27944d5e196c59b4d1e0a114f2
ZenthicMC <43224845+ZenthicMC@users.noreply.github.com>
2021-07-08 18:54:27 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
-tar -czvf /home/backup/backup-sg2-$(date +%F).tar.gz /var/lib/pterodactyl/volumes/*
-rclone copy /home/backup/backup-sg2-$(date +%F).tar.gz gdrive:sg2
-rm /home/backup/backup-sg2-$(date +%F).tar.gz
+tar -czvf /home/backup/backup-$(date +%F).tar.gz /var/lib/pterodactyl/volumes/*
+rclone copy /home/backup/backup-$(date +%F).tar.gz gdrive:backup
+rm /home/backup/backup-$(date +%F).tar.gz
 
 
